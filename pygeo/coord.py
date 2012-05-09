6deeea3cc60a2679a363cdd1e70f84d9a4c63ce8
Brendan Smithyman <brendan@bitsmithy.net>
2012-05-09 17:47:45 +0000

    Coordinate transformation codes.

---
+++ pygeo/coord.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def rotationPlan (origpoints, angle=0., about=[0.,0.,0.]):
+
+  # origpoints is a n by 3 array
+  newpoints = origpoints.copy()
+  temparr = origpoints - np.array(about)
+  newpoints[:,0] = np.cos(angle) * temparr[:,0] + np.sin(angle) * temparr[:,1] + about[0]
+  newpoints[:,1] = -np.sin(angle) * temparr[:,0] + np.cos(angle) * temparr[:,1] + about[1]
+  
+  return newpoints
+
+def reduceToLocal (origpoints, angle=0., about=[0.,0.,0.]):
+  about = np.array(about)
+  return rotationPlan(origpoints, angle, about) - about
