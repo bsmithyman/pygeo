5f8f0856999286468fd55f2aa9acba4d66ba750d
Brendan Smithyman <brendan@bitsmithy.net>
2012-06-13 23:03:21 +0000

    Explicitly placed under GNU Lesser GPL. Prior to this point, all rights were reserved.

--- pygeo/fullpy.py
+++ pygeo/fullpy.py
@@ -1,3 +1,24 @@
+
+# pygeo - a distribution of tools for managing geophysical data
+# Copyright (C) 2011, 2012 Brendan Smithyman
+
+# This file is part of pygeo.
+
+# pygeo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+
+# pygeo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with pygeo.  If not, see <http://www.gnu.org/licenses/>.
+
+# ----------------------------------------------------------------------
+
 import numpy as np
 
 def str2bool(v):
